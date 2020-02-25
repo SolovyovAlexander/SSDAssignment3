7e77900b329223391ec2f9bed63f9d79317cbcda
Alexander Trushin <twicetwix@gmail.com>
2020-02-25 21:10:29 +0300

    raw src

---
+++ src/DataTypes.java
@@ -0,0 +1,31 @@
+public class DataTypes {
+
+    public enum Status {
+        ON_VACATION,
+        BUSINESS_TRIP,
+        WORKING,
+        ABSENT,
+        USING_WEEKEND_DAYS
+    }
+
+    public enum WorkPosition {
+        PROGRAMMER,
+        MARKETER,
+        CLEANING,
+        SECURITY,
+        SEO,
+        ACCOUNTANT,
+        SCRUM_MASTER,
+        TEAMLEAD
+    }
+
+    public enum EducationLevel {
+        BA,
+        BSc,
+        MA,
+        MSc,
+        LLM,
+        PhD,
+        DPhill
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Collection<Employee> employee_storage = new LinkedList<>();
+    }
+}
