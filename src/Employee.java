42268e9f22bbdc82b1f0a11afa285db17348f4d4
Solovyov Alexander <AleksDaemon@yandex.ru>
2020-02-25 21:12:46 +0300

    smth

--- src/Employee.java
+++ src/Employee.java
@@ -1,7 +1,23 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Employee extends HumanEntity {
     int salary;
     boolean isWorking;
     String workPosition;
     String[] educationData;
     String status;
+    public String login(String phone, String password){
+        return null;
+    }
+    public void sendLocation(GPSLocation location){
+
+    }
+    public void takePhoto(){
+
+    }
+    public ArrayList<GPSLocation> getLocations(Date date){
+        return  null;
+    }
+
 }
