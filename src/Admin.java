6e972856a82298ded62bdcaa61ad498d9199e26b
Solovyov Alexander <AleksDaemon@yandex.ru>
2020-02-25 21:21:55 +0300

    smth1

---
+++ src/Admin.java
@@ -0,0 +1,11 @@
+public class Admin extends HR {
+    public Employee createEmployee(){
+        return null;
+    }
+    public Employee updateEmployee(){
+        return null;
+    }
+    public void deleteEmployee(int idNumber){
+
+    }
+}
--- src/HR.java
+++ src/HR.java
@@ -4,5 +4,8 @@ public class HR {
     public GPSLocation checkUserLocation(int idNumber, Date date){
         return null;
     }
+    public int checkSalary(int id, Date date){
+        return 0;
+    }
 
 }
